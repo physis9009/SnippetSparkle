5fdc21a176725f6334ae2000d31633e221d2196e
wenbo <9009wwb@gmail.com>
2026-05-16 21:24:26 +0800

    Improve card flow styling

--- app/ui/cardflow.tsx
+++ app/ui/cardflow.tsx
@@ -4,9 +4,14 @@ export default async function CardFlow() {
     const snippets = await fetchSnippets();
 
     return (
-        <div className="flex flex-wrap gap-2">
+        <div className="columns-1 sm:columns-2 gap-2 w-full bg-blk-md">
             {snippets.map((snippet) => (
-                <pre key={snippet.id} className='card-responsive flex-1 border border-blk-gr rounded p-4'><code className='text-xs sm:text-sm'>
+                <pre key={snippet.id} 
+                style={{ minWidth: '100%', width: '100%', maxHeight: '400px', overflowY: 'auto' }}    
+                className='
+                    card-responsive break-inside-avoid mb-2 rounded-sm p-3 block box-border bg-blk
+                    cursor-pointer border-solid border-2 border-blk-gr hover:shadow-md transition-shadow
+                '><code className='text-xs sm:text-sm whitespace-pre-wrap wrap-break-words w-full'>
                     {snippet.code}
                 </code></pre>
             ))}
--- app/ui/navigation.tsx
+++ app/ui/navigation.tsx
@@ -1,5 +1,4 @@
 import { Noto_Sans } from "next/font/google";
-import Link from "next/link";
 import NavLinks from "./nav-links"
 
 const notoSans = Noto_Sans({weight: "400", style: "normal", subsets: ["latin"]});
@@ -7,8 +6,8 @@ const notoSans = Noto_Sans({weight: "400", style: "normal", subsets: ["latin"]})
 export default function Navigation() {
     return (
         <nav className={`
-            ${notoSans.className} text-xl flex-1 border border-blk-gr 
-            w-full max-h-15 bg-blk-gr sm:h-full sm:max-w-30 sm:max-h-none
+            ${notoSans.className} text-xl flex-1 border-b-2 sm:border-r-4 border-blk-gr
+            w-full max-h-10 bg-blk-md sm:h-full sm:max-w-25 sm:max-h-none
             flex flex-row sm:flex-col justify-evenly items-center
         `}>
             <NavLinks />
